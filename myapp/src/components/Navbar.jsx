9e79850f111ff4a8667997ec1be9cc1bd0ffb8c4
Nilesh <chettrinilesh25@gmail.com>
2025-12-01 21:06:18 +0530

    fix: update dropdown menu background color for better visibility

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -122,7 +122,7 @@ const Navbar = () => {
         <div className='group relative'>
           <img src={profileIcon} className='w-5 cursor-pointer' alt='profile' />
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-800 text-gray-500 rounded'>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
               <p className='cursor-pointer hover:text-black'>Orders</p>
               <p className='cursor-pointer hover:text-black'>Logout</p>
