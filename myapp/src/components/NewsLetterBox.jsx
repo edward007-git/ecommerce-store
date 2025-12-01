313a436af290f24b8b662ad429915faffeb2d86b
Nilesh <chettrinilesh25@gmail.com>
2025-12-01 21:15:34 +0530

    fix: update placeholder text in NewsLetterBox component for clarity

--- myapp/src/components/NewsLetterBox.jsx
+++ myapp/src/components/NewsLetterBox.jsx
@@ -8,8 +8,8 @@ const NewsLetterBox = () => {
   return (
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-        <p className='text-gray-400 mt-3'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, in? 
+        <p className='text-gray-400 mt-3'>“Don’t worry, we only send awesome stuff.”
+       
 
 <from  onsubmit={onSubmitHandler}className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
     <input className='w-full sm:flex-1 outline-none'  type='email' placeholder='Enter your email ' required/>
